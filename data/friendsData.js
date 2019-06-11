7aa85070764288ec50acdbe96eb33908a70e8736
Akili1999 <sluground@gmail.com>
2019-06-11 17:54:35 -0400

    survey has bugs, beginning debugging

--- data/friendsData.js
+++ data/friendsData.js
@@ -2,17 +2,17 @@
 var friendsData = [
     {
         friendName: "Todd",
-        picture: "https://media.discordapp.net/attachments/543268235257053204/587863142416121876/kappaimeantodd.png",
+        link: "https://media.discordapp.net/attachments/543268235257053204/587863142416121876/kappaimeantodd.png",
         numbers: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
     },
     {
         friendName: "Ikumi",
-        picture: "https://cdn.discordapp.com/attachments/543283270335594516/587860045123813397/D8u8_1_W4AAvvDV.png",
+        link: "https://cdn.discordapp.com/attachments/543283270335594516/587860045123813397/D8u8_1_W4AAvvDV.png",
         number: [3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3]
     },
     {
         friendName: "Keanu",
-        picture: "https://cdn.discordapp.com/attachments/538863158320955412/587864764336832527/3544944-stagebreakout_keanureevesstageonly_cyberpunk_06092019_site.png",
+        link: "https://cdn.discordapp.com/attachments/538863158320955412/587864764336832527/3544944-stagebreakout_keanureevesstageonly_cyberpunk_06092019_site.png",
         number: [1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
     }
 ];
