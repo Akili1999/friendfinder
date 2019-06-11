f50fa83672aad39f0adaffbe58e84d058df793b3
Akili1999 <sluground@gmail.com>
2019-06-11 04:42:16 -0400

    frontend stuff and new stuff

--- data/friendsData.js
+++ data/friendsData.js
@@ -3,6 +3,21 @@ var friendsData = [
         friendName: "Beth",
         picture: "https://image.shutterstock.com/mosaic_250/2936380/613759379/stock-photo-happy-cheerful-young-woman-wearing-her-red-hair-in-bun-rejoicing-at-positive-news-or-birthday-gift-613759379.jpg",
         numbers: [3, 2, 5, 2, 4, 2, 3, 1, 4, 2]
+    },
+    {
+        friendName: "Todd",
+        picture: "https://media.discordapp.net/attachments/543268235257053204/587863142416121876/kappaimeantodd.png",
+        numbers: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    },
+    {
+        friendName: "Ikumi",
+        picture: "https://cdn.discordapp.com/attachments/543283270335594516/587860045123813397/D8u8_1_W4AAvvDV.png",
+        number: [3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3]
+    },
+    {
+        friendName: "Keanu",
+        picture: "https://cdn.discordapp.com/attachments/538863158320955412/587864764336832527/3544944-stagebreakout_keanureevesstageonly_cyberpunk_06092019_site.png",
+        number: [1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
     }
 ];
 
